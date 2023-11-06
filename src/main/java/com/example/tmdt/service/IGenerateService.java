59cc24d84bdd94b98046a2f2a2605f91ff25d220
VietAnhNguyen9 <vietanh260396@gmail.com>
2023-11-06 10:41:43 +0700

    them igenerateservice

---
+++ src/main/java/com/example/tmdt/service/IGenerateService.java
@@ -0,0 +1,13 @@
+package com.example.tmdt.service;
+
+import java.util.Optional;
+
+public interface IGenerateService<E> {
+    Iterable<E> findAll();
+
+    Optional<E> findOne(Long id);
+
+    void save(E e);
+
+    void delete(Long id);
+}
